4155434bce37d23a348b48bfe97a85e6d66a4f48
Metarya Ruparel <ruparel.metarya1988@gmail.com>
2021-12-08 04:43:58 -0500

    Added logging and commenting.

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
+"""
+Author: Metarya Ruparel
+email: msr9732@nyu.edu
+Created On: 11/26/2021
+"""
 import argparse
 
 from TransactionManager.transaction_manager import TransactionManager
 
 if __name__ == '__main__':
+    """
+    Main function used to kick off the System.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--inputdir', type=str, default='./input/', help='input file name')
     parser.add_argument('--input', type=str, default='input1', help='input file name')
